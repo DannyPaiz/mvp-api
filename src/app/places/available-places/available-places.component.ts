46b9134d3c903d85c8c86ffda8319cf457f2d3b1
danny paiz <daniel.martinez.paiz@verizon.com>
2024-10-30 22:09:25 -0400

    HTTP get to load local data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
-import { Subscription, map } from 'rxjs';
+import { Subscription, catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -15,18 +15,20 @@ import { Subscription, map } from 'rxjs';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
   // dependency injection: httpClient
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
-  subsciption: Subscription | undefined;
+  // subsciption: Subscription | undefined; // fix until const subscription = this.httpClient was added, did not need this anymore
 
   // constructor(private httpCLient: HttpClient) {}
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-
+    this.isFetching.set(true);
     // an observable that we can subscribe to trigger request. (get, ...) is valid too
-    this.httpClient
+    const subsciption = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places', {
         // encapulates data within HttpResponse.body
         observe: 'response',
@@ -34,7 +36,13 @@ export class AvailablePlacesComponent implements OnInit {
       })
       .pipe(
         // prepares data for .subscribe.next((places)=>{this.places.set(places)})
-        map((response)=>response.body?.places)
+        map((response) => response.body?.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () => new Error('Something occurred while fetching places..')
+          );
+        })
       )
       .subscribe({
         // observer object, emitted once
@@ -43,10 +51,31 @@ export class AvailablePlacesComponent implements OnInit {
           // console.log(event);
           // console.log(`places: ${response.body?.places}`)
         },
+        error: (error: Error) => {
+          // using signal
+          // console.log(error); // details of error
+          this.error.set(error.message);
+        },
+        // guaranteed to run once, when req is done
+        complete: () => {
+          this.isFetching.set(false);
+        },
       });
 
     this.destroyRef.onDestroy(() => {
-      this.subsciption?.unsubscribe();
+      subsciption?.unsubscribe();
     });
   }
+
+
+  // sets value of placeId
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient.put('http://localhost:3000/user-places', {
+      placeId: selectedPlace.id
+    })
+    .subscribe({
+      next: (resData) => console.log(resData),
+    }); // triggers the PUT request
+  }
+
 }
